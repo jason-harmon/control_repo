18181501f337d0ee9e32b60491e61d07301f4bf4
jason-harmon <49253129+jason-harmon@users.noreply.github.com>
2021-10-26 08:05:52 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,12 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  host {'web.puppet.vm':
+    ensure  => present,
+    ip      => '192.168.1.131',
+  }
+  host {'db.puppet.vm':
+    ensure  => present,
+    ip      => '192.168.1.131',
+  }
 }
